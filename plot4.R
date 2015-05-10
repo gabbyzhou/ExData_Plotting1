15da0c964010f41dc1190dd930c9bce995b284e2
Gabby0617 <enqunzhoucmu@gmail.com>
2015-05-10 03:44:44 -0400

    Making Plots

--- plot4.R
+++ plot4.R
@@ -13,16 +13,16 @@ power <- transform(power, wday=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S
 
 # generate Plot 4
 par(mfrow = c(2,2), mar = c(4,4,2,1), oma = c(0,0,2,0))
-# subpolt 1
+# subplot 1
 plot(power$wday,power$Global_active_power, type="l", xlab="", ylab="Global Active Power")
-# subpolt 2
+# subplot 2
 plot(power$wday,power$Voltage, type="l", xlab="datetime", ylab="Voltage")
-# subpolt 3
+# subplot 3
 plot(power$wday,power$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
 lines(power$wday,power$Sub_metering_2,col="red")
 lines(power$wday,power$Sub_metering_3,col="blue")
 legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=c(1,1), bty="n", cex=.5)
-# subpolt 4
+# subplot 4
 plot(power$wday,power$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
 dev.copy(png, file="plot4.png", width=480, height=480)
 dev.off()
